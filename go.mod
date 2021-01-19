bcc34da8cda07156494acbf492be8f06ae9d6ff6
Alexander Hill <me@alxhill.com>
2021-01-18 22:47:38 -0500

    Switch to gobot

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module alxhill.com/ac-ir-cmd
 
 go 1.15
 
-require (
-	periph.io/x/conn/v3 v3.6.7
-)
+require gobot.io/x/gobot v1.15.0
--- sensor/temp.go
+++ sensor/temp.go
@@ -1,40 +1,52 @@
 package sensor
 
 import (
-	"fmt"
-	"log"
-	"periph.io/x/conn/v3/i2c"
-	"periph.io/x/conn/v3/i2c/i2creg"
+	"gobot.io/x/gobot/platforms/raspi"
+	//"periph.io/x/conn/v3/i2c/i2creg"
+	//"periph.io/x/conn/v3/i2c"
+	//"periph.io/x/conn/v3/i2c/i2creg"
+	"gobot.io/x/gobot/drivers/i2c"
 )
 
 type Celcius int
 
 func GetTempCelcius() (float32, error) {
-//	//r := raspi.NewAdaptor()
-//	//tempSensor := i2c.NewSHT2xDriver(r)
-//	//
-//	//tempSensor.SetAccuracy(i2c.SHT2xAccuracyHigh)
-//	//temp, err := tempSensor.Temperature()
-//	if _, err := host.Init(); err != nil {
-//		return 0, err
-//	}
-//
-	bus, err := i2creg.Open("")
-	defer bus.Close()
+	r := raspi.NewAdaptor()
+	tempSensor := i2c.NewSHT2xDriver(r)
+
+	err := tempSensor.SetAccuracy(i2c.SHT2xAccuracyHigh)
+
 	if err != nil {
 		return 0, err
 	}
 
-	// Dev is a valid conn.Conn.
-	d := &i2c.Dev{Addr: 40, Bus: bus}
+	temp, err := tempSensor.Temperature()
 
-//	// Send a command 0x10 and expect a 5 bytes reply.
-	write := []byte{0x10}
-	read := make([]byte, 5)
-	if err := d.Tx(write, read); err != nil {
-		log.Fatal(err)
+	if err != nil {
+		return 0, err
 	}
-	fmt.Printf("%v\n", read)
 
-	return 0.0, nil
+	return temp, nil
+
+//	if _, err := host.Init(); err != nil {
+//		return 0, err
+//	}
+//
+//	bus, err := i2creg.Open("")
+//	defer bus.Close()
+//	if err != nil {
+//		return 0, err
+//	}
+
+	// Dev is a valid conn.Conn.
+//	d := &i2c.Dev{Addr: 40, Bus: bus}
+//
+////	// Send a command 0x10 and expect a 5 bytes reply.
+//	write := []byte{0x10}
+//	read := make([]byte, 5)
+//	if err := d.Tx(write, read); err != nil {
+//		log.Fatal(err)
+//	}
+//	fmt.Printf("%v\n", read)
+
 }
