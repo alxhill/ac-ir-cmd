39d08642808e0b3b4dfdba01a48305944d53e4c1
Alexander Hill <me@alxhill.com>
2021-01-19 00:08:09 -0500

    cleanup

--- sensor/temp.go
+++ sensor/temp.go
@@ -19,32 +19,9 @@ func GetTempCelcius() (float32, error) {
 	}
 
 	temp, err := tempSensor.Temperature()
-
 	if err != nil {
 		return 0, err
 	}
 
 	return temp, nil
-
-//	if _, err := host.Init(); err != nil {
-//		return 0, err
-//	}
-//
-//	bus, err := i2creg.Open("")
-//	defer bus.Close()
-//	if err != nil {
-//		return 0, err
-//	}
-
-	// Dev is a valid conn.Conn.
-//	d := &i2c.Dev{Addr: 40, Bus: bus}
-//
-////	// Send a command 0x10 and expect a 5 bytes reply.
-//	write := []byte{0x10}
-//	read := make([]byte, 5)
-//	if err := d.Tx(write, read); err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Printf("%v\n", read)
-
 }
