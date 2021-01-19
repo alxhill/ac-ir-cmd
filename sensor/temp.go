8e66d20c06a06a9bddbf79be3e74bae095631384
Alexander Hill <me@alxhill.com>
2021-01-19 00:00:20 -0500

    bus

--- sensor/temp.go
+++ sensor/temp.go
@@ -10,14 +10,14 @@ type Celcius int
 func GetTempCelcius() (float32, error) {
 	r := raspi.NewAdaptor()
 
-	tempSensor := i2c.NewSHT2xDriver(r)
-	tempSensor.Start()
-
-	err := tempSensor.SetAccuracy(i2c.SHT2xAccuracyHigh)
-
-	if err != nil {
+	tempSensor := i2c.NewSHT2xDriver(r, i2c.WithAddress(0x40))
+	if err := tempSensor.Start(); err != nil {
 		return 0, err
 	}
+
+	//if err := tempSensor.SetAccuracy(i2c.SHT2xAccuracyHigh); err != nil {
+	//	return 0, err
+	//}
 
 	temp, err := tempSensor.Temperature()
 
