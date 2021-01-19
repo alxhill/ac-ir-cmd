dd9ccb77069fe1ece544a33580ee6c633cb371d0
Alexander Hill <me@alxhill.com>
2021-01-19 00:05:24 -0500

    increase accuracy

--- sensor/temp.go
+++ sensor/temp.go
@@ -9,15 +9,14 @@ type Celcius int
 
 func GetTempCelcius() (float32, error) {
 	r := raspi.NewAdaptor()
-
 	tempSensor := i2c.NewSHT2xDriver(r, i2c.WithAddress(0x40))
 	if err := tempSensor.Start(); err != nil {
 		return 0, err
 	}
 
-	//if err := tempSensor.SetAccuracy(i2c.SHT2xAccuracyHigh); err != nil {
-	//	return 0, err
-	//}
+	if err := tempSensor.SetAccuracy(i2c.SHT2xAccuracyHigh); err != nil {
+		return 0, err
+	}
 
 	temp, err := tempSensor.Temperature()
 
